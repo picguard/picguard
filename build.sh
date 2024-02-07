b6097ac4a200cb0345f4b5d7aae100dbfb6423a0
kjxbyz <kjxbyz@163.com>
2024-02-07 23:32:48 +0800

    feat: add scripts

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+if [[ "$VERCEL_ENV" == "preview" ]] ; then
+  echo "🔥Building web app in preview environment"
+  flutter/bin/flutter build web --no-tree-shake-icons --web-renderer canvaskit;
+else
+  echo "🔥Building web app in production environment"
+  flutter/bin/flutter build web --no-tree-shake-icons --web-renderer canvaskit --base-href /watermark_assistant/;
+fi
+
+exit 0
---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+set -e
+
+if ! command -v wget >/dev/null 2>&1; then
+   echo "🟩 Installing wget"
+   yum install wget xz -y
+fi
+
+if [ ! -f "flutter_linux_3.16.9-stable.tar.xz" ]; then
+   echo "🟩 Downloading Flutter"
+   wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.16.9-stable.tar.xz
+fi
+
+if [ -d "flutter" ]; then
+   echo "🟩 Removing flutter directory"
+   rm -rf flutter;
+fi
+
+echo "🟩 Unzipping"
+mkdir flutter && tar -xf flutter_linux_3.16.9-stable.tar.xz -C flutter --strip-components 1
+
+echo "🟩 Config $PWD/flutter"
+git config --global --add safe.directory "$PWD/flutter"
+
+echo "🟩 Running ls"
+ls
+
+echo "🟩 Running flutter doctor -v"
+flutter/bin/flutter doctor -v
+
+echo "🟩 Cleaning the project..."
+flutter/bin/flutter clean
+
+echo "🟩 Install Flutter Dependencies"
+flutter/bin/flutter pub get
+
+exit 0
