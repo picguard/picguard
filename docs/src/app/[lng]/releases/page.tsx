57728c4ea33764e92a809ad635b04986b2794810
kjxbyz <kjxbyz888@outlook.com>
2024-02-17 23:16:07 +0800

    fix: update eslint

--- docs/src/app/[lng]/releases/page.tsx
+++ docs/src/app/[lng]/releases/page.tsx
@@ -40,6 +40,7 @@ export default function Releases({
 
   useEffect(() => {
     fetchData(page);
+    /* eslint-disable-next-line react-hooks/exhaustive-deps */
   }, [page]);
 
   const ShowContent = useCallback(
