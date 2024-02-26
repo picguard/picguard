1329668ad5e10db2d8eca61c79881bda7ac77335
kjxbyz <kjxbyz@163.com>
2024-02-26 23:51:25 +0800

    refactor: update docs

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -2,10 +2,7 @@ const plugin = require("tailwindcss/plugin");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   future: {
     hoverOnlyWhenSupported: true,
   },
