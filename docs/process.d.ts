0d170a0ad865cf31d4965537d9d22c5692421dfd
kjxbyz <kjxbyz888@outlook.com>
2024-03-29 12:11:40 +0800

    fix: 支持全站置灰

--- docs/process.d.ts
+++ docs/process.d.ts
@@ -5,6 +5,7 @@ declare namespace NodeJS {
     NEXT_PUBLIC_GOOGLE_ID: string;
     NEXT_PUBLIC_COOKIE_BANNER_ID: string;
     NEXT_PUBLIC_SHOW_PARTICLES: string;
+    NEXT_PUBLIC_WEBSITE_GLOBAL_GRAY: string;
     NEXT_PUBLIC_GH_TOKEN: string;
     NEXT_PUBLIC_GH_REPO_OWNER: string;
     NEXT_PUBLIC_GH_REPO: string;
