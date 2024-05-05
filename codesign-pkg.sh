d4f5c7639198af86c0b5de9bb411249239726298
kjxbyz <kjxbyz@163.com>
2024-05-05 18:31:56 +0800

    test: update test-macos-pkg.yml
    [skip ci]

---
+++ codesign-pkg.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+set -e
+
+# https://gist.github.com/GuillaumeFalourd/4efc73f1a6014b791c0ef223a023520a
+DEVELOPER_ID_APP=$1
+export "DEVELOPER_ID_APP=$DEVELOPER_ID_APP"
+
+signFile() {
+  filename=$1
+  echo "Signing FILE: $filename"
+  /usr/bin/codesign -s "$DEVELOPER_ID_APP" -f --timestamp -i "com.kjxbyz.watermarkassistant.mac" -o runtime "$filename"
+}
+
+export -f signFile
+if [ -d "build/macos/Build/Products/Release/PicGuard.app" ]; then
+    cd build/macos/Build/Products/Release
+    find PicGuard.app -exec bash -c 'signFile "{}"' \;
+else
+  echo "Directory (build/macos/Build/Products/Release/PicGuard.app) does not exist!"
+  exit 1
+fi
+
+exit 0
