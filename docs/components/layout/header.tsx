b2e418e90279f1fd9df854d220270d9a9df7ecc4
kjxbyz <kjxbyz@163.com>
2024-05-11 20:20:32 +0800

    docs: update links
    [skip ci]

--- docs/components/layout/header.tsx
+++ docs/components/layout/header.tsx
@@ -49,7 +49,7 @@ export default function Header(props: LngProps) {
             <li className="h-8 w-8 sm:h-9 sm:w-9">
               <div className="relative inline-block text-left">
                 <Link
-                  href="https://kjxbyz.com/picguard/play/"
+                  href="https://picguard.app"
                   target="_blank"
                   className="flex h-8 w-8 items-center justify-center overflow-hidden rounded-full transition-all duration-75 focus:outline-none active:scale-95 sm:h-9 sm:w-9"
                 >
