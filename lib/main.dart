09faf8fa6cf90bdf1f989df00ae5fb78bbfa859e
kjxbyz <kjxbyz@163.com>
2024-05-11 20:00:30 +0800

    refactor: update sentry config & switch to logging
    [skip ci]

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:logging/logging.dart';
 import 'package:picguard/app/navigator.dart';
 import 'package:picguard/constants/constants.dart';
 import 'package:picguard/i18n/i18n.dart';
@@ -11,6 +12,7 @@ import 'package:picguard/modules/home/home.dart';
 import 'package:picguard/theme/theme.dart';
 import 'package:picguard/utils/utils.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:sentry_logging/sentry_logging.dart';
 import 'package:window_manager/window_manager.dart';
 
 Future<void> reportErrorAndLog(FlutterErrorDetails details) async {
@@ -28,16 +30,37 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SpUtil.getInstance();
 
+  Logger.root.level = kReleaseMode ? Level.OFF : Level.ALL; // defaults to Level.INFO
+  Logger.root.onRecord.listen((record) {
+    log('${record.level.name}: ${record.time}: ${record.message}');
+  });
+
   if (PgEnv.sentryEnabled) {
     await SentryFlutter.init(
       (options) {
         options
           ..dsn = PgEnv.sentryDsn
-          ..tracesSampleRate = 1.0;
+          ..tracesSampleRate = 1.0
+          ..profilesSampleRate = 1.0
+          ..attachThreads = true
+          ..enableWindowMetricBreadcrumbs = true
+          ..addIntegration(LoggingIntegration(minEventLevel: Level.INFO))
+          ..sendDefaultPii = true
+          ..reportSilentFlutterErrors = true
+          ..attachScreenshot = true
+          ..screenshotQuality = SentryScreenshotQuality.low
+          ..attachViewHierarchy = true
+          ..debug = kDebugMode
+          ..spotlight = Spotlight(enabled: true)
+          ..enableTimeToFullDisplayTracing = true
+          ..enableMetrics = true
+          ..maxRequestBodySize = MaxRequestBodySize.always
+          ..maxResponseBodySize = MaxResponseBodySize.always
+          ..navigatorKey = AppNavigator.key;
       },
     );
   } else {
-    log('sentry is not enabled, please check the .env file');
+    printWarningLog('sentry is not enabled, please check the .env file');
   }
 
   final onError = FlutterError.onError;
@@ -65,7 +88,16 @@ Future<void> main() async {
   }
 
   LocaleSettings.useDeviceLocale(); // initialize with the right locale
-  runApp(TranslationProvider(child: const MainApp()));
+  runApp(
+    TranslationProvider(
+      child: SentryWidget(
+        child: DefaultAssetBundle(
+          bundle: SentryAssetBundle(),
+          child: const MainApp(),
+        ),
+      ),
+    ),
+  );
 }
 
 ///
