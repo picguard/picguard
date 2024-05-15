0ff3cdc2ad97f0f3ee040d16d2ede5ea684bf218
kjxbyz <kjxbyz888@outlook.com>
2024-05-15 15:30:31 +0800

    fix: update build-web.sh

--- build-web.sh
+++ build-web.sh
@@ -7,10 +7,10 @@ echo "${commit_sha:0:8}"
 
 if [[ "$VERCEL_ENV" == "preview" ]] ; then
   echo "🔥Building web app in preview environment"
-  flutter/bin/flutter build web --no-tree-shake-icons --dart-define SENTRY_DSN="$PG_ENV_STG" --build-number "${commit_sha:0:8}" --web-renderer canvaskit
+  flutter/bin/flutter build web --dart-define SENTRY_DSN="$PG_ENV_STG" --build-number "${commit_sha:0:8}"
 else
   echo "🔥Building web app in production environment"
-  flutter/bin/flutter build web --no-tree-shake-icons --dart-define SENTRY_DSN="$PG_ENV_PROD" --build-number "${commit_sha:0:8}" --web-renderer canvaskit
+  flutter/bin/flutter build web --dart-define SENTRY_DSN="$PG_ENV_PROD" --build-number "${commit_sha:0:8}"
 fi
 
 exit 0
