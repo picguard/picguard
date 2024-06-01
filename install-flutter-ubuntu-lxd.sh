9631f1433521a2edd6d247956fe06e6322ebed38
kjxbyz <kjxbyz@163.com>
2024-06-01 22:38:15 +0800

    ci(linux): update arm64 support for snap
    [skip ci]

--- install-flutter-ubuntu-lxd.sh
+++ install-flutter-ubuntu-lxd.sh
@@ -9,18 +9,18 @@ fi
 
 PG_FLUTTER_BUILD_DIR="$CRAFT_PART_BUILD/flutter-distro"
 
-if [ -d "$PG_FLUTTER_BUILD_DIR" ]; then
-   echo "🟩 Removing flutter directory"
-   rm -rf "$PG_FLUTTER_BUILD_DIR"
-fi
-
 if [ ! -f "$CRAFT_PART_BUILD/flutter-3.19.6-aarch64.tar.xz" ]; then
    echo "🟩 Downloading Flutter"
    wget https://github.com/containerbase/flutter-prebuild/releases/download/3.19.6/flutter-3.19.6-aarch64.tar.xz -P "$CRAFT_PART_BUILD"
 fi
 
+if [ -d "$PG_FLUTTER_BUILD_DIR" ]; then
+   echo "🟩 Removing flutter directory"
+   rm -rf "$PG_FLUTTER_BUILD_DIR"
+fi
+
 echo "🟩 Unzipping"
-mkdir -p "$PG_FLUTTER_BUILD_DIR" && tar -xf flutter-3.19.6-aarch64.tar.xz -C "$PG_FLUTTER_BUILD_DIR" --strip-components 1
+mkdir -p "$PG_FLUTTER_BUILD_DIR" && tar -xf "$CRAFT_PART_BUILD/flutter-3.19.6-aarch64.tar.xz" -C "$PG_FLUTTER_BUILD_DIR" --strip-components 1
 
 echo "🟩 Config $PG_FLUTTER_BUILD_DIR"
 git config --global --add safe.directory "$PG_FLUTTER_BUILD_DIR"
