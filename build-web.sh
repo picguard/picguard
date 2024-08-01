3e6b64bcaefba746bfdec84a1ccaac967679b316
kjxbyz <kjxbyz888@outlook.com>
2024-08-01 15:58:29 +0800

    Update build-web.sh

--- build-web.sh
+++ build-web.sh
@@ -4,7 +4,7 @@ set -e
 
 if [[ "$VERCEL_ENV" == "preview" ]] ; then
   echo "🔥Building web app in preview environment"
-  flutter/bin/flutter build web --dart-define SENTRY_DSN="$PG_ENV_STG"
+  flutter/bin/flutter build web --dart-define SENTRY_DSN="$PG_ENV_DEV"
 else
   echo "🔥Building web app in production environment"
   echo "VERCEL_GIT_COMMIT_SHA: $VERCEL_GIT_COMMIT_SHA"
