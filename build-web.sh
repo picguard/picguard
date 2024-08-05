95231354731c310468ca0ff19ab110f7c7fb39af
kjxbyz <kjxbyz888@outlook.com>
2024-08-05 21:50:02 +0800

    Added web deployment for pro version

--- build-web.sh
+++ build-web.sh
@@ -9,8 +9,14 @@ else
   echo "🔥Building web app in production environment"
   echo "VERCEL_GIT_COMMIT_SHA: $VERCEL_GIT_COMMIT_SHA"
   COMMIT_COUNT="$(git rev-list --count $VERCEL_GIT_COMMIT_SHA)"
-  echo "COMMIT_COUNT: $COMMIT_COUNT"
-  flutter/bin/flutter build web --dart-define SENTRY_DSN="$PG_ENV_PROD" --build-number "$COMMIT_COUNT" --base-href /play/
+  echo "COMMIT_COUNT: $COMMIT_COUNT, FLAVOR: $FLAVOR"
+  if [[ "$FLAVOR" == "pro" ]] ; then
+    echo "🔥Built for pro version"
+    flutter/bin/flutter build web --dart-define SENTRY_DSN="$PG_ENV_PROD" --target lib/main_pro.dart --build-number "$COMMIT_COUNT" --base-href /play/pro/
+  else
+    echo "🔥Built for free version"
+    flutter/bin/flutter build web --dart-define SENTRY_DSN="$PG_ENV_PROD" --target lib/main_free.dart --build-number "$COMMIT_COUNT" --base-href /play/
+  fi
 fi
 
 exit 0
