f3bbeaffd40caa50c1eb33e6c0edb40f2db0d51c
kjxbyz <kjxbyz888@outlook.com>
2024-08-15 11:27:12 +0800

    chore: v1.0.0+390

--- scripts/build-web.sh
+++ scripts/build-web.sh
@@ -7,15 +7,12 @@ if [[ "$VERCEL_ENV" == "preview" ]] ; then
   flutter/bin/flutter build web --dart-define SENTRY_DSN="$PG_ENV_DEV"
 else
   echo "🔥Building web app in production environment"
-  echo "VERCEL_GIT_COMMIT_SHA: $VERCEL_GIT_COMMIT_SHA"
-  COMMIT_COUNT="$(git rev-list --count $VERCEL_GIT_COMMIT_SHA)"
-  echo "COMMIT_COUNT: $COMMIT_COUNT, FLAVOR: $FLAVOR"
   if [[ "$FLAVOR" == "pro" ]] ; then
     echo "🔥Built for pro version"
-    flutter/bin/flutter build web --dart-define SENTRY_DSN="$PG_ENV_PROD" --target lib/main_pro.dart --build-number "$COMMIT_COUNT" --base-href /play/pro/
+    flutter/bin/flutter build web --dart-define SENTRY_DSN="$PG_ENV_PROD" --target lib/main_pro.dart --base-href /play/pro/
   else
     echo "🔥Built for free version"
-    flutter/bin/flutter build web --dart-define SENTRY_DSN="$PG_ENV_PROD" --target lib/main_free.dart --build-number "$COMMIT_COUNT" --base-href /play/
+    flutter/bin/flutter build web --dart-define SENTRY_DSN="$PG_ENV_PROD" --target lib/main_free.dart --base-href /play/
   fi
 fi
 
