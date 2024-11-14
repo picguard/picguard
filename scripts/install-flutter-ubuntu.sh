8199f7bb28a10e5217e2df48c868bf4a3d6ca02f
kjxbyz <kjxbyz888@outlook.com>
2024-11-14 12:50:09 +0800

    Building from source on Linux arm64

--- scripts/install-flutter-ubuntu.sh
+++ scripts/install-flutter-ubuntu.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-if ! command -v wget >/dev/null 2>&1; then
-   echo "🟩 Installing wget"
-   sudo apt install wget xz-utils -y
-fi
-
 if ! command -v git >/dev/null 2>&1; then
    echo "🟩 Installing git"
    sudo apt install git -y
@@ -19,23 +14,15 @@ if [ ! -d "$FLUTTER_DIR" ]; then
    mkdir -p "$FLUTTER_DIR"
 fi
 
-if [ ! -f "$FLUTTER_DIR/flutter-3.24.4-aarch64.tar.xz" ]; then
-   echo "🟩 Downloading Flutter"
-   wget https://github.com/containerbase/flutter-prebuild/releases/download/3.24.4/flutter-3.24.4-aarch64.tar.xz -P "$FLUTTER_DIR"
-fi
-
 if [ -d "$FLUTTER_DIR/flutter" ]; then
    echo "🟩 Removing flutter directory"
    rm -rf "$FLUTTER_DIR/flutter"
 fi
 
-echo "🟩 Unzipping"
-mkdir "$FLUTTER_DIR/flutter" && tar -xf "$FLUTTER_DIR/flutter-3.24.4-aarch64.tar.xz" -C "$FLUTTER_DIR/flutter" --strip-components 1
+echo "🟩 Cloning"
+mkdir -p "$FLUTTER_DIR/flutter" && git clone --depth 1 -b 3.24.4 https://github.com/flutter/flutter.git "$FLUTTER_DIR/flutter"
 
 echo "🟩 Config $FLUTTER_DIR/flutter"
 git config --global --add safe.directory "$FLUTTER_DIR/flutter"
-
-echo "🟩 Running ls"
-ls
 
 exit 0
