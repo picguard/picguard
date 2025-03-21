c7c81defccbdb474fcee5301b12a6c6fa8ef6188
kjxbyz <kjxbyz888@outlook.com>
2025-03-22 02:29:27 +0800

    refactor: update build-dmg.sh

--- scripts/build-dmg.sh
+++ scripts/build-dmg.sh
@@ -9,20 +9,26 @@ fi
 
 create-dmg --version
 
-test -f PicGuard.dmg && rm -f PicGuard.dmg
+APP_NAME="PicGuard"
+BASE_APP_DIR="$APP_NAME.app"
+APP_FILE_NAME="$APP_NAME.dmg"
+PACK_DIR="macos/packaging"
+TARGET_DIR="build/macos/Build/Products/Release-free"
+
+test -f "$APP_FILE_NAME" && rm -f "$APP_FILE_NAME"
 
 create-dmg \
-  --volname "PicGuard" \
-  --volicon "logo/logo.icns" \
-  --background "images/background.png" \
-  --eula "eula.rtf" \
+  --volname "$APP_NAME" \
+  --volicon "$PACK_DIR/dmg/logo.icns" \
+  --background "$PACK_DIR/dmg/background.png" \
+  --eula "$PACK_DIR/base/eula.rtf" \
   --window-pos 200 180 \
   --window-size 660 500 \
   --icon-size 100 \
-  --icon "PicGuard.app" 180 170 \
-  --hide-extension "PicGuard.app" \
+  --icon "$BASE_APP_DIR" 180 170 \
+  --hide-extension "$BASE_APP_DIR" \
   --app-drop-link 480 170 \
-  "PicGuard.dmg" \
-  "build/macos/Build/Products/Release-free/PicGuard.app"
+  "$APP_FILE_NAME" \
+  "$TARGET_DIR/$BASE_APP_DIR"
 
 exit 0
