8e3e10b0bbd6dac186b294f75bcbeff8c2257fcf
kjxbyz <kjxbyz888@outlook.com>
2025-05-10 00:00:57 +0800

    fix(web): update icons for pro version

--- scripts/build-web-netlify.sh
+++ scripts/build-web-netlify.sh
@@ -7,9 +7,12 @@ echo "CONTEXT: $CONTEXT"
 echo "FLAVOR: $FLAVOR"
 echo "APP_PREVIEW: $APP_PREVIEW"
 
+echo "🔥Building web app in $CONTEXT environment"
+
 if [ "$CONTEXT" = "production" ]; then
-  echo "🔥Building web app in production environment"
   if [ "$FLAVOR" = "pro" ]; then
+    echo "🔥Copy $(pwd)/web/pro/ to $(pwd)/web/"
+    cp -r $(pwd)/web/pro/* "$(pwd)/web/"
     echo "🔥Built for pro version"
     flutter build web --dart-define SENTRY_DSN="$PG_ENV_PROD" --dart-define GIT_COMMIT_SHA="$COMMIT_REF" --dart-define APP_PREVIEW="$APP_PREVIEW" --target lib/main_pro.dart
   else
@@ -17,8 +20,9 @@ if [ "$CONTEXT" = "production" ]; then
     flutter build web --dart-define SENTRY_DSN="$PG_ENV_PROD" --dart-define GIT_COMMIT_SHA="$COMMIT_REF" --dart-define APP_PREVIEW="$APP_PREVIEW" --target lib/main_free.dart
   fi
 else
-  echo "🔥Building web app in $CONTEXT environment"
   if [ "$FLAVOR" = "pro" ]; then
+    echo "🔥Copy $(pwd)/web/pro/ to $(pwd)/web/"
+    cp -r $(pwd)/web/pro/* "$(pwd)/web/"
     echo "🔥Built for pro version"
     flutter build web --dart-define SENTRY_DSN="$PG_ENV_STG" --dart-define GIT_COMMIT_SHA="$COMMIT_REF" --dart-define APP_PREVIEW="$APP_PREVIEW" --target lib/main_pro.dart
   else
--- scripts/build-web-vercel.sh
+++ scripts/build-web-vercel.sh
@@ -7,9 +7,12 @@ echo "VERCEL_ENV: $VERCEL_ENV"
 echo "FLAVOR: $FLAVOR"
 echo "APP_PREVIEW: $APP_PREVIEW"
 
+echo "🔥Building web app in $VERCEL_ENV environment"
+
 if [ "$VERCEL_ENV" = "production" ]; then
-  echo "🔥Building web app in production environment"
   if [ "$FLAVOR" = "pro" ]; then
+    echo "🔥Copy $(pwd)/web/pro/ to $(pwd)/web/"
+    cp -r $(pwd)/web/pro/* "$(pwd)/web/"
     echo "🔥Built for pro version"
     flutter/bin/flutter build web --dart-define SENTRY_DSN="$PG_ENV_PROD" --dart-define GIT_COMMIT_SHA="$VERCEL_GIT_COMMIT_SHA" --dart-define APP_PREVIEW="$APP_PREVIEW" --target lib/main_pro.dart --base-href /play/pro/
   else
@@ -17,8 +20,9 @@ if [ "$VERCEL_ENV" = "production" ]; then
     flutter/bin/flutter build web --dart-define SENTRY_DSN="$PG_ENV_PROD" --dart-define GIT_COMMIT_SHA="$VERCEL_GIT_COMMIT_SHA" --dart-define APP_PREVIEW="$APP_PREVIEW" --target lib/main_free.dart --base-href /play/
   fi
 else
-  echo "🔥Building web app in $VERCEL_ENV environment"
   if [ "$FLAVOR" = "pro" ]; then
+    echo "🔥Copy $(pwd)/web/pro/ to $(pwd)/web/"
+    cp -r $(pwd)/web/pro/* "$(pwd)/web/"
     echo "🔥Built for pro version"
     flutter/bin/flutter build web --dart-define SENTRY_DSN="$PG_ENV_DEV" --dart-define GIT_COMMIT_SHA="$VERCEL_GIT_COMMIT_SHA" --dart-define APP_PREVIEW="$APP_PREVIEW" --target lib/main_pro.dart
   else
