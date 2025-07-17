fecc341f29c725ae5f7424edb291c2c420ab6750
kjxbyz <kjxbyz888@outlook.com>
2025-07-17 22:15:55 +0800

    chore(deps): update deps

--- lib/i18n/i18n.g.dart
+++ lib/i18n/i18n.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 3
 /// Strings: 258 (86 per locale)
 ///
-/// Built on 2025-04-24 at 08:54 UTC
+/// Built on 2025-07-17 at 14:15 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
